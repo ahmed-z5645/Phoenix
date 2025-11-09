aa8c88e5e5dbd70bbc157974aff8ce0ee4c7eccf
ahmed-z5645 <ahmed.zafar5645@gmail.com>
2025-11-09 16:01:44 -0500

    trying to find errors

--- backend/app/routes/sms.py
+++ backend/app/routes/sms.py
@@ -12,12 +12,20 @@ async def receive_sms(request: Request):
     form = await request.form()
     body = form.get("Body")
     sender = form.get("From")
-    
+
     try:
         data = json.loads(body)
         incident = Incident(**data) #unpacking i'm lowkey goated at python (sorry judges im not concieted, im just excited)
+        print("1")
+        print(incident)
         incident.id = f"INC{uuid.uuid4().hex[:6].upper()}"
-        supabase.table("incidents").insert(incident.dict()).execute()
+        print("2")
+        print(incident.id)
+        print("3")
+        print(incident)
+        response = supabase.table("incidents").insert(incident.dict()).execute()
+        print("4")
+        print(response)
         return {"status": "success",
                 "id": incident.id}
     except (json.JSONDecodeError, ValidationError) as e:
